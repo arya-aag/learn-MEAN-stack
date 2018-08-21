5b1a546a423738243e464352a35c27c48e9e0a28
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2018-08-22 00:02:08 +0530

    fetch list from database

--- mean-app/backend/app.js
+++ mean-app/backend/app.js
@@ -17,10 +17,7 @@ mongoose.connect('mongodb://max_mean:max123mean@127.0.0.1:27017/max-mean').then(
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept'
-  );
+  res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
@@ -33,33 +30,16 @@ app.post('/api/posts', (req, res, next) => {
     content: req.body.content
   });
   post.save();
-  // console.log(post);
   res.status(201).json({ msg: 'success' });
 });
 
 app.use('/api/posts', (req, res, next) => {
-  Post.find(posts => {});
-
-  const posts = [
-    {
-      id: '156asd1f65sdaf5',
-      title: 'First',
-      details: 'This is the askjdfak j khkjhasf  lskajh saklfjda.'
-    },
-    {
-      id: '156asdfsaf5',
-      title: 'Second',
-      details: 'This is the askjd4356758985678562kajh saklfjda.'
-    },
-    {
-      id: '156a24353245daf5',
-      title: 'Third',
-      details: 'This iaasdfw34534564356f  lskajh saklfjda.'
-    }
-  ];
-  res.status(200).json({
-    msg: 'success',
-    data: posts
+  Post.find().then(posts => {
+    console.log(posts);
+    res.status(200).json({
+      msg: 'success',
+      data: posts
+    });
   });
 });
 
