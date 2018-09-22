3d68fadf4985ea391e1e9ddaddd0144ab2a1d983
Arya Adarsha Gautam <arya-aag@users.noreply.github.com>
2018-09-22 17:57:13 +0530

    create basic signup/in forms

--- mean-app/src/app/app-routing.module.ts
+++ mean-app/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 const routes: Routes = [
   { path: '', component: PostsListComponent },
   { path: 'create', component: CreatePostComponent },
-  { path: 'edit/:postId', component: CreatePostComponent }
+  { path: 'edit/:postId', component: CreatePostComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: SigninComponent }
 ];
 
 @NgModule({
--- mean-app/src/app/app.module.ts
+++ mean-app/src/app/app.module.ts
@@ -1,29 +1,39 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppMaterialModule } from './app-material.module';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
-import { PostService } from './posts/posts.service';
-import { AppRoutingModule } from './app-routing.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 @NgModule({
-  declarations: [AppComponent, PostsComponent, CreatePostComponent, PostsListComponent],
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    CreatePostComponent,
+    PostsListComponent,
+    SignupComponent,
+    SigninComponent
+  ],
   imports: [
     BrowserModule,
+    FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     AppMaterialModule,
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [PostService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
