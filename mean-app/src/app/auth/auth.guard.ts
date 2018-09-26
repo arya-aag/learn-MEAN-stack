a9cfadb209e92a2df35ec1db2165db149495eb75
Arya Adarsha Gautam <arya-aag@users.noreply.github.com>
2018-09-26 21:00:12 +0530

    alternate approach to redirection

--- mean-app/src/app/auth/auth.guard.ts
+++ mean-app/src/app/auth/auth.guard.ts
@@ -8,19 +8,22 @@ import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 import { AuthService } from './auth.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private authSrv: AuthService, private router: Router) {}
 
-  async canActivate(
+  canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Promise<boolean> {
-    const isAuth = await this.authSrv.tryServerAutoLogin().toPromise();
-    if (!isAuth) {
-      this.router.navigate(['/signin']);
-    }
-    return isAuth;
+  ): Observable<boolean> {
+    return this.authSrv.tryServerAutoLogin().pipe(
+      tap((isAuth: boolean) => {
+        if (!isAuth) {
+          this.router.navigate(['/signin']);
+        }
+      })
+    );
   }
 }
