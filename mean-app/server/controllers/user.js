9ef3e0549fe957c79d6dcee326730b4facd96f37
Arya Adarsha Gautam <arya-aag@users.noreply.github.com>
2018-10-05 10:17:54 +0530

    move server-side users logic to controller

---
+++ mean-app/server/controllers/user.js
@@ -0,0 +1,77 @@
+// library imports
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// project imports
+const User = require('../models/user');
+const checkAuth = require('../middleware/check-auth');
+const SECRET_KEY = checkAuth.secret;
+
+// code
+exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: 'User created!',
+          payload: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: 'Invalid Authentication Credentials!',
+          payload: err
+        });
+      });
+  });
+};
+
+exports.loginUser = (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: 'Invalid Credentials: Check email.',
+          payload: null
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: 'Invalid Credentials: Check password.',
+          payload: null
+        });
+      }
+      const token = jwt.sign(
+        {
+          email: fetchedUser.email,
+          userId: fetchedUser._id
+        },
+        SECRET_KEY,
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({
+        message: 'Authenticated!',
+        payload: { token, expiresIn: 3600, userId: fetchedUser._id }
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: 'Invalid Authentication Credentials!',
+        payload: err
+      });
+    });
+};
+
+exports.tryAutoLogin = (req, res, next) => {
+  res.status(200).json({ message: 'auto login success', payload: true });
+};
--- mean-app/server/routes/users.js
+++ mean-app/server/routes/users.js
@@ -1,90 +1,15 @@
 // library imports
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-// project imports
-const User = require('../models/user');
-const checkAuth = require('../middleware/check-auth');
-
-// code
 const router = express.Router();
 
-const SECRET_KEY = checkAuth.secret;
+// project imports
+const userController = require('../controllers/user');
+const checkAuth = require('../middleware/check-auth');
 const checker = checkAuth.checker;
 
 // routes
-
-// get
-router.get('/autologin', checker, (req, res, next) => {
-  res.status(200).json({ message: 'auto login success', payload: true });
-});
-
-// post
-router.post('/signup', (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then(hash => {
-    const user = new User({
-      email: req.body.email,
-      password: hash
-    });
-    user
-      .save()
-      .then(result => {
-        res.status(201).json({
-          message: 'User created!',
-          payload: result
-        });
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: 'Invalid Authentication Credentials!',
-          payload: err
-        });
-      });
-  });
-});
-
-router.post('/login', (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({
-          message: 'Invalid Credentials: Check email.',
-          payload: null
-        });
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(result => {
-      if (!result) {
-        return res.status(401).json({
-          message: 'Invalid Credentials: Check password.',
-          payload: null
-        });
-      }
-      const token = jwt.sign(
-        {
-          email: fetchedUser.email,
-          userId: fetchedUser._id
-        },
-        SECRET_KEY,
-        { expiresIn: '1h' }
-      );
-      res.status(200).json({
-        message: 'Authenticated!',
-        payload: { token, expiresIn: 3600, userId: fetchedUser._id }
-      });
-    })
-    .catch(err => {
-      return res.status(401).json({
-        message: 'Invalid Authentication Credentials!',
-        payload: err
-      });
-    });
-});
-// put
-// delete
+router.get('/autologin', checker, userController.tryAutoLogin);
+router.post('/signup', userController.createUser);
+router.post('/login', userController.loginUser);
 
 module.exports = router;
